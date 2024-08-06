db08ae362c6f325c3a4cba5e5342ee6f800f8ca6
sriramyogi99 <109013675+sriramyogi99@users.noreply.github.com>
2024-08-06 12:39:29 +0530

    Update AddCommentForm.js

--- client/src/commentModules/AddCommentForm.js
+++ client/src/commentModules/AddCommentForm.js
@@ -4,7 +4,7 @@ import '../styles/Comment.css';
 
 const AddCommentForm = ({ postId, userId, parentId, commentId, commentDescription, onCommentAdded, value, depth }) => {
   const [comment, setComment] = useState(commentDescription || "");
-  const server_url = "https://user-post-comment-chat-project.onrender.com";
+  const server_url = "https://user-post-comment-chat-project-backend.onrender.com";
 
   useEffect(() => {
     setComment(commentDescription || "");
