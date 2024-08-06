e4128b9b75ca27d951b4ce77baf52f9e44ffc566
sriramyogi99 <109013675+sriramyogi99@users.noreply.github.com>
2024-08-06 13:05:29 +0530

    Update Input.js

--- client/src/chatModules/Input.js
+++ client/src/chatModules/Input.js
@@ -5,16 +5,24 @@ const Input = ({
     name = '',
     type = 'text',
     className = '',
-    inputClassName = '',
     isRequired = true,
     placeholder = '',
     value = '',
     onChange = () => {},
-}) => {
+  }) => {
   return (
-    <div className={`${className}`}>
-        <label for={name} className="block text-sm font-medium text-gray-800">{label}</label>
-        <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${inputClassName}`} placeholder={placeholder} required={isRequired} value={value} onChange={onChange} />
+    <div className={className}>
+      {label && <label htmlFor={name} className="block text-sm font-medium text-gray-800">{label}</label>}
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        required={isRequired}
+        value={value}
+        onChange={onChange}
+        className="input-field"
+      />
     </div>
   )
 }
