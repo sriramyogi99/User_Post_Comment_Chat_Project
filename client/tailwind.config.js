e636920cccfefe1edc95cb241aa1eae4c7424254
sriramyogi99 <109013675+sriramyogi99@users.noreply.github.com>
2024-08-06 12:16:37 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/chatModules/**/*.{js,jsx,ts,tsx}",
+    // "./src/chatModules/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
